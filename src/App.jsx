871a130f78f571884e859cbdc48faac4f5324418
TAYYABA ZAHRA <127332101+Tayyabazahra@users.noreply.github.com>
2025-03-04 23:59:38 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
-import Login from '../src/pages/Login/Login'
-function App() {
-  
+import React from 'react';
+import './App.css';
+import Login from './Pages/Login/Login';
+import Signup from './Pages/Signup/Signup';
+import Home from './Pages/Home/Home';
+import { Routes,Route} from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
+function App() {
   return (
-      <Login />
-  )
+    <div className="App">
+     <Routes>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
