c19d7b47b1496fca4727b71890c2098bb9937751
AleezaMahsood <aleeza.mahsood18@gmail.com>
2025-03-05 17:12:33 +0500

    Changes Committed

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import './App.css';
 import Login from './Pages/Login/Login';
 import Signup from './Pages/Signup/Signup';
 import Home from './Pages/Home/Home';
-import { Routes,Route} from 'react-router-dom';
+import { Routes,Route, BrowserRouter} from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
     <div className="App">
-     <Routes>
+      <BrowserRouter>
+      <Routes>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/signup' element={<Signup/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
       </Routes>
+      </BrowserRouter>
     </div>
   );
 }
