3594ff216b504c74d864969095d6f6b25cebf72d
Tayyabazahra <127332101+Tayyabazahra@users.noreply.github.com>
2025-03-20 23:38:17 +0500

    Changes Committed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,19 +2,16 @@ import React from "react";
 import JobList from "../../components/Jobcard/JobList";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Navigation from "../../components/Navigation/Navigation";
-import "./Home.css"; // Import CSS
+import "./Home.css"; 
 
 const Home = () => {
   return (
     <div className="home-container">
-      {/* Fixed Navigation */}
       <Navigation />
 
       <div className="content-container">
-        {/* Fixed Sidebar */}
         <Sidebar />
 
-        {/* Scrollable Job List */}
         <div className="joblist-container">
           <JobList />
         </div>
